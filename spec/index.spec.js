a9d6a3587c99b220d2b2025be0ee2835380a31e8
FatimataAliouSall <sallfamaaliou@gmail.com>
2024-10-01 13:10:56 +0200

    Initialize new Git repository and remove previous versioning

---
+++ spec/index.spec.js
@@ -0,0 +1,84 @@
+import Recipe from '../src/models/RecipeModel.js';
+
+describe('Recipe tests', () => {
+  let recipeId = null;
+
+  it('can be created', async () => {
+    const recipe = {
+      title: 'crepe',
+      type: 'dessert',
+      description: 'pâte à base de farine',
+      ingredient: 'farine',
+    };
+
+    const result = await Recipe.create(
+      recipe.title,
+      recipe.type,
+      recipe.description,
+      recipe.ingredient
+    );
+    recipeId = result.insertId;
+    const recipeCreated = await Recipe.getById(recipeId);
+
+    expect(recipeId).not.toBeNull();
+    expect(recipeCreated).not.toBeNull();
+    expect(recipeCreated.title).toBe(recipe.title);
+  });
+
+  it('can get all recipes', async () => {
+    const getAll = await Recipe.getAll();
+    expect(getAll).not.toBeNull();
+    expect(Array.isArray(getAll)).toBe(true);
+  });
+
+  it('can update a recipe', async () => {
+    const updatedData = {
+      title: 'updated crepe',
+      type: 'dessert',
+      description: 'updated description',
+      ingredient: 'updated ingredient',
+    };
+
+    const result = await Recipe.update(recipeId, updatedData);
+    expect(result.affectedRows).toBe(1);
+
+    const updatedRecipe = await Recipe.getById(recipeId);
+    expect(updatedRecipe.title).toBe(updatedData.title);
+    expect(updatedRecipe.description).toBe(updatedData.description);
+    expect(updatedRecipe.ingredient).toBe(updatedData.ingredient);
+  });
+
+  it('cannot create recipe with invalid data', async () => {
+    const recipe = {
+      titre: null,
+      ingredients: 'Lait, Chocolat, sucre',
+      description: 'updated description',
+      type: 'Dessert',
+    };
+
+    try {
+      await Recipe.createRecipe(
+        recipe.titre,
+        recipe.ingredients,
+        recipe.description,
+        recipe.type
+      );
+      fail('Expected an error to be thrown');
+    } catch (error) {
+      console.log('Erreur  :', error.message);
+    }
+  });
+
+  it('can delete a recipe', async () => {
+    const result = await Recipe.delete(recipeId);
+    expect(result.affectedRows).toBe(1);
+    const deletedRecipe = await Recipe.getById(recipeId);
+    expect(deletedRecipe).toBeNull();
+  });
+
+  it('Cannot get recipe by invalid ID', async () => {
+    const invalidId = 9999;
+    const recipe = await Recipe.getById(invalidId);
+    expect(recipe).toBeNull();
+  });
+});
---
+++ src/validators/recipeValidat.js
@@ -0,0 +1,106 @@
+import { check, param, validationResult } from 'express-validator';
+import { StatusCodes } from 'http-status-codes';
+import Recipe from '../models/RecipeModel.js ';
+
+const handleValidationErrors = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res
+      .status(StatusCodes.UNPROCESSABLE_ENTITY)
+      .json({ errors: errors.array() });
+  }
+  next();
+};
+
+const addRequestValidator = [
+  check('title')
+    .not()
+    .isEmpty()
+    .withMessage('Le titre ne peut pas être vide.')
+    .bail()
+    .isLength({ min: 6 })
+    .withMessage('Le titre doit comporter au moins 6 caractères.')
+    .bail()
+    .custom(async (value) => {
+      const existingRecipe = await Recipe.checkRecipe(value);
+      if (existingRecipe) {
+        throw new Error('Cette recette existe déjà.');
+      }
+      return true;
+    }),
+  handleValidationErrors,
+];
+
+const deleteRequestValidator = [
+  param('id')
+    .not()
+    .isEmpty()
+    .withMessage('L/ID est obligatoire.')
+    .bail()
+    .custom(async (value) => {
+      const recipe = await Recipe.getById(value);
+      if (!recipe) {
+        throw new Error("Cette recette n'existe pas.");
+      }
+      return true;
+    }),
+  handleValidationErrors,
+];
+
+const updateRequestValidator = [
+  param('id')
+    .not()
+    .isEmpty()
+    .withMessage("L'ID de la recette est requis.")
+    .bail()
+    .custom(async (value) => {
+      const recipe = await Recipe.getById(value);
+      if (!recipe) {
+        throw new Error("Cette recette n'existe pas.");
+      }
+      return true;
+    }),
+  check('title')
+    .optional()
+    .isLength({ min: 6 })
+    .withMessage('Le titre doit contenir au moins 6 caractères.'),
+  check('type')
+    .optional()
+    .not()
+    .isEmpty()
+    .withMessage('Le type de recette est requis.'),
+  check('ingredient')
+    .optional()
+    .not()
+    .isEmpty()
+    .withMessage('Les ingrédients sont requis.'),
+  check('description')
+    .optional()
+    .not()
+    .isEmpty()
+    .withMessage('La description est requise.'),
+  handleValidationErrors,
+];
+
+const getByIdRequestValidator = [
+  param('id')
+    .not()
+    .isEmpty()
+    .withMessage("L'ID de la recette est requis.")
+    .bail()
+    .custom(async (value) => {
+      const recipe = await Recipe.getById(value);
+      if (!recipe) {
+        throw new Error("Cette recette n'existe pas.");
+      }
+      return true;
+    }),
+  handleValidationErrors,
+];
+
+export {
+  addRequestValidator,
+  deleteRequestValidator,
+  updateRequestValidator,
+  getByIdRequestValidator,
+};
